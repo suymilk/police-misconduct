70fdc5afb7b28a35e9af32ab7d639452f9bff550
Suyeon Son <suyeon.son@icloud.com>
2014-04-15 20:18:52 -0500

    get user input for all unwanted phrases

--- 2012sustained/sparser.py
+++ 2012sustained/sparser.py
@@ -1,10 +1,13 @@
 from __future__ import print_function
 import fileinput
 
+phrases = []
 filename = "sustained012012.txt"
-phrase = "Created by INDEPENDENT POLICE REVIEW AUTHORITY"
+phrases.extend(raw_input("Enter the phrases you want to get rid of, separated by commas: ").split(", "))
+print (phrases)
 
-for line in fileinput.input(filename, inplace=True):
-	if phrase in line:
-		continue
-	print(line, end="")
+for phrase in phrases:
+	for line in fileinput.input(filename, inplace=True):
+		if phrase in line:
+			continue
+		print(line, end="")
