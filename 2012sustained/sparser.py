d09ece6d6b88b083de5571ff14d7adf08f3d37e1
Suyeon Son <suyeon.son@icloud.com>
2014-04-15 21:10:16 -0500

    user input for file name, the offending phrases

--- 2012sustained/sparser.py
+++ 2012sustained/sparser.py
@@ -1,13 +1,18 @@
 from __future__ import print_function
 import fileinput
+import pdb
 
 phrases = []
-filename = "sustained012012.txt"
-phrases.extend(raw_input("Enter the phrases you want to get rid of, separated by commas: ").split(", "))
+filename = raw_input("Enter filename with extension: ")
+
+phrases.extend(raw_input("Enter the phrases you want to get rid of, separated by double commas: ").split(",,"))
+# for phrase in phrases:
+# 	phrases = phrase.lower()
 print (phrases)
 
 for phrase in phrases:
 	for line in fileinput.input(filename, inplace=True):
+		# line = line.lower()
 		if phrase in line:
 			continue
 		print(line, end="")
