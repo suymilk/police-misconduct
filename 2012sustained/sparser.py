f6a3d59eb3c037c8dfd2b257b263d798c2af6a23
Suyeon Son <suyeon.son@icloud.com>
2014-04-21 15:21:35 -0500

    latest version
    hi nicole

--- 2012sustained/sparser.py
+++ 2012sustained/sparser.py
@@ -1,18 +1,32 @@
 from __future__ import print_function
 import fileinput
+import csv
 import pdb
 
-phrases = []
+writer = csv.writer(open("2012sustaineddata.csv", "w"))
+writer.writerow(["Log #", "Date of complaint filing", "Incident date", "Location", "Allegations", "Conclusions", "Recommendations"])
+
+# Declare which file to work with
 filename = raw_input("Enter filename with extension: ")
 
-phrases.extend(raw_input("Enter the phrases you want to get rid of, separated by double commas: ").split(",,"))
-# for phrase in phrases:
-# 	phrases = phrase.lower()
-print (phrases)
+with open(filename, "r+") as f:
+	data = f.read()
+	f.seek(0)
+	f.write(" ".join(line.strip() for line in f))
 
-for phrase in phrases:
-	for line in fileinput.input(filename, inplace=True):
-		# line = line.lower()
-		if phrase in line:
-			continue
-		print(line, end="")
+# f = open(filename, "r")
+# data = f.read()
+# paragraphs = data.split("\n\n")
+# print (paragraphs)
+
+# Removing unwanted phrases from document for cleanup
+# phrases = []
+# phrases.extend(raw_input("Enter the phrases you want to get rid of, separated by commas: ").split(","))
+# print (phrases)
+
+# for phrase in phrases:
+# 	for line in fileinput.input(filename, inplace=True):
+# 		if phrase in line:
+# 			continue
+# 		print(line, end="")
+
