5ab5e6298b3dbfe8127c9667790ccec41afb486b
Suyeon Son <suyeon.son@icloud.com>
2014-04-24 15:39:44 -0500

    split by logs, remove newline characters

--- 2012sustained/sparser.py
+++ 2012sustained/sparser.py
@@ -4,5 +4,16 @@ f = open("sustained012012.txt")
 content = str(f.read())
 
 content = re.split("\n(?=Log)", content)
-print content
+# print content
+
+# mycontent = map(lambda each:each.strip("\n"), content)
+
+allcontent = []
+
+allcontent = [el.replace("\n", " ") for el in content]
+
+
+o = open("out.txt", "w")
+print >>o, allcontent
+
 
