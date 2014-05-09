5a6e11db02a6e3e35ee1e3ccd7a5878dab50651a
Suyeon Son <suyeon.son@icloud.com>
2014-05-09 13:31:11 -0500

    focus on districts

--- 2012sustained/sparser.py
+++ 2012sustained/sparser.py
@@ -1,51 +1,59 @@
-import re, glob, os, sys, itertools
+import re, glob, os, sys, itertools, sys
 from collections import Counter
 
-for file in glob.glob("*.txt"):
-	with open(file) as content:
-		content = content.read()
+remove_words = ["Created by INDEPENDENT POLICE REVIEW AUTHORITY", "Abstracts of Sustained Cases", "2012", "Page"]
 
-# remove_words = ["Created by INDEPENDENT POLICE REVIEW AUTHORITY", "Abstracts of Sustained Cases", "2012", "Page"]
+read_files = glob.glob("*.txt")
 
-# with open(filename) as oldfile, open ('newfile.txt', 'w') as newfile:
-# 	for line in oldfile:
-# 		if not any(remove_word in line for remove_word in remove_words):
-# 			newfile.write(line)
+input = "2012sustainedALL.txt"
+output = "2012sustainedCLEAN.txt"
 
-# f = open("newfile.txt")
+with open(input, "wb") as outfile:
+    for f in read_files:
+        with open(f, "rb") as infile:
+            outfile.write(infile.read())
+
+fIn = open(input)
+fOut = open(output, "w")
+
+for line in fIn:
+	if not True in [word in line for word in remove_words]:
+		fOut.write(line)
+
+content = open(output, "r")
+content = str(content.read())
 
 # split file by each incident (starts with "Log")
-	content = re.split("\n(?=Log)", content)
+content = re.split("\n(?=Log)", content)
 
-	allcontent = []
+allcontent = []
 
-	# join line breaks caused by pdftotext
-	allcontent = [log.replace("\n", " ") for log in content]
+# join line breaks caused by pdftotext
+allcontent = [log.replace("\n", " ") for log in content]
 
-	# all files start with unncessary title that gets grouped into own element; delete element
-	del allcontent[0]
+# all files start with unncessary title that gets grouped into own element; delete element
+del allcontent[0]
 
-	# what's this thing look like?
-	# o = open("out.txt", "w")
-	# print >>o, allcontent
+# what's this thing look like?
+# o = open("out.txt", "w")
+# print >>o, allcontent
 
-	# find all Log/C.R. numbers, store to logno
-	for log in allcontent:
-		logno = re.findall(r"No. (\d+)", log)
-		# print logno
+# find all Log/C.R. numbers, store to logno
+for log in allcontent:
+	logno = re.findall(r"No. (\d+)", log)
+	# print logno
 
-	# find all dates on which the complaint was filed
-	for log in allcontent: 
-		datefiled = re.findall(r"\d+\sOn\s([A-z]+\s\d+,\s\d+)", log)
-		# print datefiled
+# find all dates on which the complaint was filed
+for log in allcontent: 
+	datefiled = re.findall(r"\d+\sOn\s([A-z]+\s\d+,\s\d+)", log)
+	# print datefiled
 
-	# find the district in which the incident occurred
-	district = []
-	alldistrict = []
-	for log in allcontent:
-		district.append(re.findall(r"(\d+)\D+ District", log))
+# find the district in which the incident occurred
+district = []
+alldistrict = []
+for log in allcontent:
+	district.append(re.findall(r"(\d+)\D+ District", log))
 
-	# district = list(itertools.chain.from_iterable(district))
-	print district
-	# print Counter(alldistrict)
-
+# district = list(itertools.chain.from_iterable(district))
+print district
+# print Counter(alldistrict)
